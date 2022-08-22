a4860ec6d060f1fbb1ae8675ff47892f3ca731f6
mahmud987 <safemahmud987@gmail.com>
2022-08-22 23:02:44 +0600

    some text eddit

--- js/calculate.js
+++ js/calculate.js
@@ -30,9 +30,14 @@ document.getElementById('calculate').addEventListener('click', function () {
     const palyerListFieldAmount = document.getElementById('player-list').childElementCount;
     const perPlayerCost = getInputValueById('per-player-cost');
     if (isNaN(perPlayerCost)) {
-        alert('input a number')
+        alert('input a number for per player');
+        return;
     }
     const playersExpence = palyerListFieldAmount * perPlayerCost;
+    if (playersExpence == 0) {
+        alert('select atleast one player');
+        return;
+    }
     const setPlayersExpence = setElementValue('players-expence', playersExpence);
     // console.log(playersExpence);
 
@@ -44,11 +49,13 @@ document.getElementById('btn-calculate-total').addEventListener('click', functio
     const playersExpence = getelementsValueById('players-expence');
     const manegerExpence = getInputValueById('Manegar-cost');
     if (isNaN(manegerExpence)) {
-        alert('input a number')
+        alert('input a number for maneger');
+        return;
     };
     const coachExpence = getInputValueById('coach-cost');
     if (isNaN(coachExpence)) {
-        alert('input a number')
+        alert('input a number for coach');
+        return;
     };
 
     const TotalExpence = playersExpence + manegerExpence + coachExpence;
